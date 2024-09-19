7965a9794982d8a7b01370837c836e634a8a10a4
Slenderino <135389069+Slenderino@users.noreply.github.com>
2024-09-18 23:41:47 -0300

    Update exec.cmd

--- exec.cmd
+++ exec.cmd
@@ -1,4 +1,4 @@
 python-3.12.6-embed-amd64\python.exe get-pip.py
-set PATH=%PATH%%~dp0python-3.12.6-embed-amd64\Scripts\
+set PATH=%PATH%%~dp0python-3.12.6-embed-amd64\Scripts\;
 python-3.12.6-embed-amd64\Scripts\pip.exe install -r requirements.txt
 python-3.12.6-embed-amd64\python.exe main.py
